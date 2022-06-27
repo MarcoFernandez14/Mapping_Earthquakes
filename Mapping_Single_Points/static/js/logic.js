65123dcc4edfcde3cd943ed619816fbd9c1e4227
Marco FERNANDEZ <fernanm@mh.lvmh>
2022-06-27 07:51:38 -0700

    Adding Leaflet markers

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 // let map = L.map("mapid", {
 //     center: [
@@ -11,6 +11,21 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //     zoom: 4
 //   });
 
+//  Add a marker (circle) to the map for Los Angeles, California.
+
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// let marker = L.circle([34.0522, -118.2437], {
+//             radius: 300,
+//             color: 'black',
+//             fillColor: 'yellow',
+//             fillOpacity: 0.5
+//             }).addTo(map);
+
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: 'black',
+    fillColor: '#ffffa1'
+    }).addTo(map);
 
 
 // We create the tile layer that will be the background of our map.
@@ -33,7 +48,7 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 
 // Option: Use the Mapbox Styles API
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
