52341710b8731de02b0a5c07bcaa9c0b5a324a41
Marco FERNANDEZ <fernanm@mh.lvmh>
2022-06-29 08:11:08 -0700

    Adding Linestrings code

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -3,7 +3,7 @@ console.log("working");
 
 
 // We create the tile layer that will be the background of our map. Option: Use the Mapbox Styles API
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -32,7 +32,7 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Street: streets,
+    Light: light,
     Dark: dark
 };
 
@@ -45,9 +45,9 @@ let baseMaps = {
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [30, 30],
+    center: [44.0, -80.0],
     zoom: 2,
-    layers: [streets]
+    layers: [dark]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -93,17 +93,24 @@ L.control.layers(baseMaps).addTo(map);
 //    }
 // }).addTo(map);
 
-// Accessing the airport GeoJSON URL. Having the tileLayer() method before accessing large datasets ensures that the map gets loaded before the data is added to it.
-let airportData = "https://raw.githubusercontent.com/MarcoFernandez14/Mapping_Earthquakes/main/majorAirports.json";
+// Accessing the Toronto airline routes GeoJSON URL. Having the tileLayer() method before accessing large datasets ensures that the map gets loaded before the data is added to it.
+let torontoData = "https://raw.githubusercontent.com/MarcoFernandez14/Mapping_Earthquakes/main/torontoRoutes.json";
+
+// Create a style for the lines.
+let myStyle = {
+    color: "#ffffa1",
+    weight: 2
+}
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
     console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
+    style: myStyle,
     onEachFeature: function(feature, layer) {
         console.log(layer);
-        layer.bindPopup("<h2>" + "Airport code: " + feature.properties.faa + "</h2> <hr> <h3>" + "Airport name: " + feature.properties.name + "</h3>");
+        layer.bindPopup("<h3>" + "Airline: " + feature.properties.airline + "</h3> <hr> <h3>" + "Destination: " + feature.properties.dst + "</h3>");
         }
     }).addTo(map);
 });
